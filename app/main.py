17e25f36d734067f5d6c49aeec2f8c9b9ec979ce
Yuliana <pashkova.yuliana@gmail.com>
2024-07-18 13:48:13 +0300

    add new dir for app

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from app.database import Base, engine
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
